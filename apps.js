d898591fb364a012c85767d4f99e56eadd507b7e
Jake A. Smith <theman@jakeasmith.com>
2013-07-03 01:59:09 -0400

    First draft

---
+++ apps.js
@@ -0,0 +1,89 @@
+
+function AppListController($scope) {
+	$scope.apps = [
+		{
+			"name": "CodeKit",
+			"site": "http://incident57.com/codekit",
+			"file": "http://incident57.com/codekit/files/codekit-8300.zip",
+			"tags": ['javascript', 'less', 'css']
+		},
+		{
+			"name": "Dropbox",
+			"site": "https://www.dropbox.com",
+			"file": "https://www.dropbox.com/downloading?os=mac",
+			"tags": ['file', 'storage', 'sync']
+		},
+		{
+			"name": "Google Chrome",
+			"site": "https://www.google.com/intl/en/chrome/browser/",
+			"file": "https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg",
+			"tags": ['browser']
+		},
+		{
+			"name": "iTerm 2",
+			"site": "http://www.iterm2.com/#/section/home",
+			"file": "http://iterm2.com/downloads/stable/iTerm2_v1_0_0.zip",
+			"tags": ['terminal']
+		},
+		{
+			"name": "Firefox",
+			"site": "http://www.mozilla.org/en-US/firefox/new/",
+			"file": "https://download.mozilla.org/?product=firefox-22.0&os=osx&lang=en-US",
+			"tags": ['browser']
+		},
+		{
+			"name": "Git",
+			"site": "http://git-scm.com/",
+			"file": "https://git-osx-installer.googlecode.com/files/git-1.8.3.2-intel-universal-snow-leopard.dmg",
+			"tags": ['source control']
+		},
+		{
+			"name": "MySQL Server",
+			"site": "http://www.mysql.com/",
+			"file": "http://cdn.mysql.com/Downloads/MySQL-5.6/mysql-5.6.12-osx10.7-x86_64.dmg",
+			"tags": ['database', 'server']
+		},
+		{
+			"name": "Opera",
+			"site": "http://www.opera.com/",
+			"file": "http://www.opera.com/download/get/?partner=www&opsys=MacOS",
+			"tags": ['browser']
+		},
+		{
+			"name": "Skype",
+			"site": "http://www.skype.com/en/",
+			"file": "http://www.skype.com/en/download-skype/skype-for-mac/downloading/",
+			"tags": ['chat']
+		},
+		{
+			"name": "Spotify",
+			"site": "https://www.spotify.com/us/",
+			"file": "http://download.spotify.com/SpotifyInstaller.zip",
+			"tags": ['music']
+		},
+		{
+			"name": "Sequel Pro",
+			"site": "http://www.sequelpro.com",
+			"file": "https://sequel-pro.googlecode.com/files/sequel-pro-1.0.2.dmg",
+			"tags": ['mysql']
+		},
+		{
+			"name": "Sublime Text 2",
+			"site": "http://sublimetext.com",
+			"file": "http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1.dmg",
+			"tags": ['editor', 'code', 'IDE']
+		},
+		{
+			"name": "Transmit",
+			"site": "http://panic.com/transmit/",
+			"file": "http://www.panic.com/transmit/d/Transmit%204.4.zip",
+			"tags": ['ftp', 's3', 'webdav']
+		},
+		{
+			"name": "Viscosity",
+			"site": "http://www.sparklabs.com/viscosity/",
+			"file": "http://www.sparklabs.com/downloads/Viscosity.dmg",
+			"tags": ['vpn']
+		}
+	];
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+# AppDL
+Just a tool for quickly downloading all of the mac apps a developer needs to get up and running. Right now this is just a quick and dirty list I compiled while bringing up a new machine. If you have an app that should be on the list please add it to the `apps.js` file and submit a pull request. Thanks to all of the app developer out there who make it possible for me to do my job on a daily basis.
+
+# Roadmap
+* Get more apps
+* Make it pretty
+* Create some sort of downloader (copy / paste is lame)
+* Build in some sort of functionality for saved profiles
+* Maybe someday add some instructions to run the installers and do some cleanup afterwards
